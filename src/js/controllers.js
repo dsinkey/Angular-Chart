dff7ecce7884199ee519e843996555af98a0e636
Dustin <dustinsinkey1@gmail.com>
2016-01-19 22:04:46 -0800

    The 'I wish I knew how to spell better commit'

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,6 +1,28 @@
 'use strict';
 
 angular.module('myApp.controllers', []).controller('IssueCtrl', function ($scope, $http) {
+
+	$http.get('https://api.github.com/repos/angular/angular.js/issues?state=open&sort=updated&page=1&per_page=100&assignee=*').success(function(issues){
+		var assignees = [];
+		var data = [];
+
+		issues.forEach(function(issue){
+			assignees.push({category: issue.assignee.login});
+		});
+
+		assignees = _.groupBy(assignees, function(assignee){return assignee.category; });
+
+		assignees = _.sortBy(assignees, function(assignee){return assignee.length; }).reverse();
+
+		assignees = assignees.slice(0, 10);
+
+		assignees.forEach(function(assignee){
+			data.push({catergory: assignee[0].category, value: assignee.length});
+		});
+
+		$scope.data = data;
+	});
+
 	$scope.x = function(){
 		return function(d){
 			return d.catergory;
@@ -12,29 +34,4 @@ angular.module('myApp.controllers', []).controller('IssueCtrl', function ($scope
 			return d.value;
 		};
 	};
-
-	$http.get('https://api.github.com/repos/angular/angular.js/issues?state=open&sort=updated&page=1&per_page=100&assignee=*')
-		.success(function(issues){
-			var assignees = [];
-			var data = [];
-
-			issues.forEach(function(issue){
-				assignees.push({catergory: issue.assignee.login});
-			});
-
-			assignees = _.groupBy(assignees, function(assignee){
-				return assignee.catergory;
-			});
-
-			assignees = _.sortBy(assignees, function(assignee){
-				return assignee.length; }).reverse();
-
-			assignees = assignees.slice(0, 10);
-
-			assignees.forEach(function(assignee){
-				data.push({catergory: assignee[0].catergory, value: assignee.length});
-			});
-
-			$scope.data = data;
-		});
 });
