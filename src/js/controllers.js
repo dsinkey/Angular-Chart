474967cfc6cf1f9461e6aeceaff189e65abfb12e
Dustin <dustinsinkey1@gmail.com>
2016-01-20 21:19:57 -0800

    Fixed errors

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -31,28 +31,27 @@ angular.module('myApp.controllers', []).controller('IssueCtrl', function ($scope
 			return d.value;
 		};
 	};
-}).controller('PullCtrl', function ($scope, $htpp){
+}).controller('PullCtrl', function ($scope, $http){
 	$scope.loadData = function(){
-		$htpp.get('https://api.github.com/repos/angular/angular.js/pulls?state=closed&sort=updated&page=' + $scope.page + '&per_page=100&sort=updated').success(function(pulls){
+		$http.get('https://api.github.com/repos/angular/angular.js/pulls?state=closed&sort=updated&page=' + $scope.page + '&per_page=100&sort=updated').success(function(pulls){
 			var pulley = [];
 			var data = [];
 
-			pull.forEach(function(pull){
-				pulley.push({valley: new Date(pull.updated_at).getDay()});
+			pulls.forEach(function(pull){
+				pulley.push({value: new Date(pull.updated_at).getDay()});
 			});
 			pulley = _.groupBy(pulley, function(pull){ return pull.value; });
 			pulley = _.sortBy(pulley, function(pull, i){ return pull[0].value; });
-			pulley = assignees.slice(0, 10);
 			pulley.forEach(function(pull, i){
 				data.push([i, pull.length]);
 			});
 			$scope.data = [{
 				"key": "Sun - Sat",
-				"values": loadData
+				"values": data
 			}];
 		});
 	};
-	
+
 	$scope.page = 1;
 	$scope.loadData();
 });
